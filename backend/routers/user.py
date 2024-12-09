f18ca9673aec823fd08ad19879afd2ef5a6307bb
msepulvedaD <msepulveda.ap@dimacofi.cl>
2024-12-09 17:01:46 -0300

    aa

--- backend/routers/user.py
+++ backend/routers/user.py
@@ -58,7 +58,7 @@ async def login_for_access_token(
         )
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.name}, expires_delta=access_token_expires
+        data={"sub": user.id}, expires_delta=access_token_expires
     )
     return Token(access_token=access_token, token_type="bearer")
 
